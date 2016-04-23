5ad1f7b0fa4e88a25db6b57ce4790a7a1bb86d13
Robert Saylor <robert@customphpdesign.com>
2016-04-22 20:14:17 -0400

    development

--- class/reports.php
+++ class/reports.php
@@ -83,7 +83,8 @@ class Reports {
 			`c`.`firstname`,
 			`c`.`lastname`,
 			`c`.`email`,
-			`lc`.`name` AS 'card_name'
+			`lc`.`name` AS 'card_name',
+			`lc`.`created_at`
 
 
 
@@ -107,10 +108,10 @@ class Reports {
 
 		print "<h3>Loyalty Stamps</h3>";
 		print "<table class=\"table\">";
-		print "<tr><td>Application</td><td>Employee</td><td>Points</td><td>First Name</td><td>Last Name</td><td>Email</td><td>Card Name</td></tr>";
+		print "<tr><td>Created</td><td>Application</td><td>Employee</td><td>Points</td><td>First Name</td><td>Last Name</td><td>Email</td><td>Card Name</td></tr>";
 		$result = $this->new_mysql($sql);
 		while ($row = $result->fetch_assoc()) {
-			print "<tr><td>$row[app_name]</td><td>$row[employee]</td><td>$row[number_of_points]</td><td>$row[firstname]</td><td>$row[lastname]</td><td>$row[email]</td><td>$row[card_name]</td></tr>";
+			print "<tr><td>$row[created_at]</td><td>$row[app_name]</td><td>$row[employee]</td><td>$row[number_of_points]</td><td>$row[firstname]</td><td>$row[lastname]</td><td>$row[email]</td><td>$row[card_name]</td></tr>";
 		}
 		print "</table>";
 	}
