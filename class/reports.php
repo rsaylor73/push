8a833b9124c6f8b5b2bf5261dd0e2cb845890b93
Robert Saylor <robert@customphpdesign.com>
2016-04-26 11:00:24 -0400

    development

--- class/reports.php
+++ class/reports.php
@@ -167,12 +167,12 @@ class Reports {
 
 		header('Content-Description: File Transfer');
 		header('Content-Type: application/octet-stream');
-		header('Content-Disposition: attachment; filename=file.csv'));
+		header('Content-Disposition: attachment; filename=file.csv');
 		header('Content-Transfer-Encoding: binary');
 		header('Expires: 0');
 		header('Cache-Control: must-revalidate, post-check=0, pre-check=0');
 		header('Pragma: public');
-		//echo "\xEF\xBB\xBF"; // UTF-8 BOM
+		echo "\xEF\xBB\xBF"; // UTF-8 BOM
 
 		
 	}
